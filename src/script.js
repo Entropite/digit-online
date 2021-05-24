a8d709193f805b2e0ce42b6871201216bff95a2d
Entropite <41209768+Entropite@users.noreply.github.com>
2021-05-24 00:06:15 -0400

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function run() {
-    prog = document.getElementById("program_input").value;
+    prog = document.getElementById("program_input").value.trim();
     //clear output
     document.getElementById("program_output").innerText = ""
     
@@ -13,57 +13,54 @@ function run() {
     let keyRegister = 4
     let cycles = 5000
 
-    
-    process = function(){while (index < prog.length){
-        cycles--;
-        if (cycles < 0){
-            if (confirm("Your program is taking quite a while to complete, would you like to keep it running or suspend it?")){
-                break;
+    while (index < prog.length){
+            cycles--;
+            if (cycles < 0){
+                if (confirm("Your program is taking quite a while to complete, would you like to keep it running or suspend it?")){
+                    break;
+                }
             }
-        }
-        if(prog[index] in registers){
-            keyRegister = parseInt(prog[index], 10)
-        }else{
-            switch(prog[index]){
-                case "0":
-                    if(registers[keyRegister] == 0){
-                        index += 2
-                    }
-                    break;
-                case "1":
-                    registers[keyRegister]++;
-                    break;
-                case "2":
-                    registers[keyRegister]--;
-                    break;
-                case "3":
-                    index -= registers[keyRegister];
-                    break;
-                case "9":
-                    document.getElementById("program_output").innerText += "\n" + registers[keyRegister];
-                    break;
-                case ".":
-                    let userInput = NaN
-                    while (true){
-                        userInput = parseInt(prompt("Input an integer value", "0"))
-                        if (!isNaN(userInput)){
-                            break;
+            if(prog[index] in registers){
+                keyRegister = parseInt(prog[index], 10)
+            }else{
+                switch(prog[index]){
+                    case "0":
+                        if(registers[keyRegister] == 0){
+                            index += 2
                         }
-                        alert("Please submit a valid integer response");
+                        break;
+                    case "1":
+                        registers[keyRegister]++;
+                        break;
+                    case "2":
+                        registers[keyRegister]--;
+                        break;
+                    case "3":
+                        index -= registers[keyRegister];
+                        break;
+                    case "9":
+                        document.getElementById("program_output").innerText += "\n" + registers[keyRegister];
+                        break;
+                    case ".":
+                        let userInput = NaN
+                        while (true){
+                            userInput = parseInt(prompt("Input an integer value", "0"))
+                            if (!isNaN(userInput)){
+                                break;
+                            }
+                            alert("Please submit a valid integer response");
 
-                    }
-                    registers[keyRegister] = userInput;
+                        }
+                        registers[keyRegister] = userInput;
 
-                    break;
-                default:
-                    alert("Error: Improper command at index "+index);
-                    break;
+                        break;
+                    default:
+                        break;
+                }
             }
-        }
+        
         index++;
 
     }
-    }
 
-    setTimeout(process, 0)
 }
